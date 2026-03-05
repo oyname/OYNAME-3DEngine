d04509582a357fbef5b7469236f2678999d47499
oyname <zamansiz74@gmail.com>
2026-03-05 07:26:46 +0100

    delete unused files

--- _include/main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <windows.h>
-
-// Your window proc + your engine/game thread entry
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-int main(LPVOID hwnd);
--- _src/ShadowMapTarget.cpp
+++
@@ -1,8 +0,0 @@
-#include "ShadowMapTarget.h"
-
-// Schritt 2:
-// ShadowMapTarget ist nur noch ein Tag/Handle.
-// Alle API-Calls (Bind/Clear/Unbind/Viewport/SRV-Hazard) werden im Backend erledigt.
-//
-// Diese Datei bleibt bewusst bestehen, damit alte Projektdateien, die ShadowMapTarget.cpp
-// noch in der Build-Liste haben, weiterhin kompilieren (copy + redirect).
